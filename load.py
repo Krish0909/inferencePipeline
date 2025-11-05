c43536fd10e374e98f93ae5629cd990c7c5a4aea
Arjun Prakash <arjunprakash027@gmail.com>
2025-11-05 23:54:32 +0000

    renames

--- load.py
+++ load.py
@@ -3,8 +3,7 @@ import time
 import pandas as pd
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from inferencePipeline_llamaCpp import loadPipeline
-#from inferencePipeline import loadPipeline
+from inferencePipeline import loadPipeline
 
 # Environment settings
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
